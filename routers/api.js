9cd4d46c6a5d0b99f85f1c1542abc53959d2253a
Dominick <Dominick>
2017-03-31 14:54:13 +0800

    修复middleware顺序问题

--- routers/api.js
+++ routers/api.js
@@ -8,76 +8,44 @@ const passport = require('koa-passport')
 const User = require ('../models/User')
 const bcrypt = require('bcryptjs');
 const config = require('../config')
-const sendEmail = require('../lib/sendEmail')
 const jwt = require('jsonwebtoken');
 const koaJwt = require('koa-jwt')
 const convert = require('koa-convert')
+function registerRoutes(router){
+    router.get('/login',function*(next){
+        console.log('login')
+        this.body = yield render('login')
 
-router.get('/login',function*(next){
-    console.log('login')
-    this.body = yield render('login')
-
-})
-router.post('/login',function *(next){
-    let body = this.request.body
-
-    let password = body.password,email = body.email
-    let user = yield User.findOne({
-        where:{
-            email:email
-        },
-        raw:true
     })
-    if(!user)this.throw('用户不存在！',400)
+    router.post('/login',function *(next){
+        let body = this.request.body
 
-    if(!bcrypt.compareSync(password, user.password))this.throw('密码错误！',400)
-    // this.body =
-    let token = jwt.sign({id:user.id},config.api.jwtSecret,{expiresIn:'1h'})
-    this.body = token
+        let password = body.password,email = body.email
+        let user = yield User.findOne({
+            where:{
+                email:email
+            },
+            raw:true
+        })
+        if(!user)this.throw('用户不存在！',400)
 
-})
+        if(!bcrypt.compareSync(password, user.password))this.throw('密码错误！',400)
+        // this.body =
+        let token = jwt.sign({id:user.id},config.api.jwtSecret,{expiresIn:'1h'})
+        this.body = token
 
+    })
+    router.get('/post',function*(next){
 
-router.get('/register',function *(next){
-    this.body = yield render('register')
-})
-router.get('/send',function *(next){
-    let mailOptions = {
-        to: 'example@qq.com', // list of receivers
-        subject: 'send mail example', // Subject line
-        text: 'Hi, I am text body', // plain text body
-        html: '<a href="http://www.baidu.com">Hi, I am html body</a>' // html body
-    };
-    let result = yield sendEmail(mailOptions)
+        this.body = this.state.user
 
-    this.body = result
-})
+    })
+}
 
-router.post('/register',function *(next){
-    // console.log(this.request.body)
-    this.checkBody('email').isEmail('email format not correct')
-    this.checkBody('password').notEmpty().len(3,20)
-    let body = this.request.body
-
-    var salt = bcrypt.genSaltSync(10);
-    var hash = bcrypt.hashSync(body.password, salt);
-    let ctx = this
-    yield User.sync().then(function () {
-        // Table created
-        return User.create({
-            email: body.email,
-            password: hash
-        });
-    }).then(function(){
-        ctx.body = 'ok'
-    }).catch(()=>{
-        ctx.body = '用户已存在'
-        // ctx.throw(401)
-    });
-
-})
 
 module.exports = function(app){
-    app.use(convert(router.routes())).use(convert(router.allowedMethods()))
+
     app.use(koaJwt({secret:config.api.jwtSecret}).unless({path:['/login']}))
+    registerRoutes(router)
+    app.use(convert(router.routes())).use(convert(router.allowedMethods()))
 }
--- routers/web.js
+++ routers/web.js
@@ -12,93 +12,97 @@ const sendEmail = require('../lib/sendEmail')
 const utils = require('../lib/utils')
 const uuid = require('uuid')
 const convert = require('koa-convert')
-//仅为测试使用。///////////////////////////////////
 
-router.get('/register',function *(next){
-    this.body = yield render('register')
-})
-//////////////////////////////////////////////////////////////
+function registerRoutes(router){
+    //仅为测试使用。///////////////////////////////////
+    router.get('/register',function *(next){
+        this.body = yield render('register')
+    })
+    //////////////////////////////////////////////////////////////
 
-router.post('/register',function *(next){
-    // console.log(this.request.body)
+    router.post('/register',function *(next){
+        // console.log(this.request.body)
 
-    let body = this.request.body
-    let email = body.email
-    if(!utils.validateEmail(email))this.throw('邮件格错误！',400)
-    let code = uuid.v4()
-    let href = 'http://localhost:3000/web/setPassword?code='+encodeURIComponent(code)
-    let mailOptions = {
-        to: email, // list of receivers
-        subject: '设置密码', // Subject line
-        html: '<a href="'+href+'" target="_blank">打开设置密码</a>' // html body
-    };
-    try{
-        var result = yield sendEmail(mailOptions)
-    }catch(err){
-        console.log(err)
-        return this.throw('发送邮件出错',500)
-    }
-    let ctx = this
-    yield RegistrationCode.sync({force:true}).then(function () {
-        // Table created
-        return RegistrationCode.create({
-            email: email,
-            code:code
+        let body = this.request.body
+        let email = body.email
+        if(!utils.validateEmail(email))this.throw('邮件格错误！',400)
+        let code = uuid.v4()
+        let href = 'http://localhost:3000/web/setPassword?code='+encodeURIComponent(code)
+        let mailOptions = {
+            to: email, // list of receivers
+            subject: '设置密码', // Subject line
+            html: '<a href="'+href+'" target="_blank">打开设置密码</a>' // html body
+        };
+        try{
+            var result = yield sendEmail(mailOptions)
+        }catch(err){
+            console.log(err)
+            return this.throw('发送邮件出错',500)
+        }
+        let ctx = this
+        yield RegistrationCode.sync({force:true}).then(function () {
+            // Table created
+            return RegistrationCode.create({
+                email: email,
+                code:code
+            });
+        }).then(function(){
+            ctx.body = result
+        }).catch((err)=>{
+            console.log(err)
+            ctx.throw(500)
         });
-    }).then(function(){
-        ctx.body = result
-    }).catch((err)=>{
-        console.log(err)
-        ctx.throw(500)
-    });
 
-})
-
-router.get('/setPassword',function *(){
-    let query = this.query
-    let code = query.code
-
-    let target = yield RegistrationCode.findOne({
-        where:{
-            code:code,
-            activated:0,
-        },
-        raw:true
     })
-    if(!target)this.throw('code不正确或已失效，请重新注册',400)
 
-    this.body = yield render('setPassword')
-})
+    router.get('/setPassword',function *(){
+        let query = this.query
+        let code = query.code
 
-router.post('/setPassword',function *(){
-    let query = this.query
-    let code = query.code
-    let target = yield RegistrationCode.findOne({
-        where:{
-            code:code,
-            activated:0,
-        },
-        raw:true
-    })
-    if(!target)this.throw('code不正确或已失效，请重新注册',400)
-    //密码长度：6-20位
-    let body = this.request.body
-    let password = body.password,repeat = body.repeat
-    if(password !==repeat)this.throw('密码不一致！',400)
-    if(password.length<6||password.length>20)this.throw('密码长度为6-20',400)
-
-    var salt = bcrypt.genSaltSync(10);
-    var hash = bcrypt.hashSync(password, salt);
-    let user = yield User.sync({force:true}).then(function(){
-        return User.create({
-            email: target.email,
-            password:hash
+        let target = yield RegistrationCode.findOne({
+            where:{
+                code:code,
+                activated:0,
+            },
+            raw:true
         })
-    })
-    console.log(user)
-    this.body = user
+        if(!target)this.throw('code不正确或已失效，请重新注册',400)
 
-})
+        this.body = yield render('setPassword')
+    })
+
+    router.post('/setPassword',function *(){
+        let query = this.query
+        let code = query.code
+        let target = yield RegistrationCode.findOne({
+            where:{
+                code:code,
+                activated:0,
+            },
+            raw:true
+        })
+        if(!target)this.throw('code不正确或已失效，请重新注册',400)
+        //密码长度：6-20位
+        let body = this.request.body
+        let password = body.password,repeat = body.repeat
+        if(password !==repeat)this.throw('密码不一致！',400)
+        if(password.length<6||password.length>20)this.throw('密码长度为6-20',400)
+
+        var salt = bcrypt.genSaltSync(10);
+        var hash = bcrypt.hashSync(password, salt);
+        let user = yield User.sync({force:true}).then(function(){
+            return User.create({
+                email: target.email,
+                password:hash
+            })
+        })
+        console.log(user)
+        this.body = user
+
+    })
+}
+
 module.exports = function(app){
+    registerRoutes(router)
     app.use(convert(router.routes())).use(convert(router.allowedMethods()))
 }
